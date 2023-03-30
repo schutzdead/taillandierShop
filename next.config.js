aa75ee398d39e13d96890c03b8fc7e2b46eb1722
Lucas <lucas.soubry@gmail.com>
2023-03-30 15:00:25 +0200

    fixed out

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/taillandierShop/' : '',
-  images: {
-    unoptimized: true,
+  output: 'export',
+  basePath: '/taillandierShop',
+  experimental: {
+    appDir: true,
   },
 }
+
+module.exports = nextConfig
