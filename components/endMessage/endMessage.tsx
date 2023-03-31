975f3b0901c901845897b54bf537f6c6b0ec1068
Lucas <lucas.soubry@gmail.com>
2023-03-31 17:59:29 +0200

    scroll

--- components/endMessage/endMessage.tsx
+++ components/endMessage/endMessage.tsx
@@ -26,7 +26,7 @@ export function EndMessage ({endDisplay}:endMessage) {
     return (
         <div className={styles.global}>
             <div className={styles.container}>
-                <Link href={document.location.origin} 
+                <Link href="/../" 
                       className={styles.back}
                       onClick={reloadCart}>
                     <Image
