761cf0255ba569f8d6e6b835886ca9148a497b57
Lucas <lucas.soubry@gmail.com>
2023-03-31 13:40:29 +0200

    configProd

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/taillandierShop/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
